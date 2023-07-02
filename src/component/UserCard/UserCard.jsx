272893edfffeaffc2d7f6a9318d6185167e4095d
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-07-02 20:16:44 +0300

    add choice

--- src/component/UserCard/UserCard.jsx
+++ src/component/UserCard/UserCard.jsx
@@ -2,8 +2,15 @@ import css from "./UserCard.module.css";
 import logo from "../../images/logo.svg";
 import bgImg from "../../images/pictureToUserCard2x.png";
 import photoImg from "../../images/boy2x.png";
+import { useState } from "react";
 
 export const UserCard = () => {
+  const [subscription, setSubscription] = useState(false);
+
+  const handleClick = () => {
+    subscription ? setSubscription(false) : setSubscription(true);
+  };
+
   return (
     <div className={css.card}>
       <img className={css.logo} src={logo} alt="logo" />
@@ -13,8 +20,12 @@ export const UserCard = () => {
       <div className={css.infoBlock}>
         <p className={css.textTweet}>777 Tweets</p>
         <p className={css.textFollowers}>100500 Followers</p>
-        <button className={css.btnFollow} type="button">
-          Follow
+        <button
+          className={!subscription ? css.btnFollow : css.btnFollowing}
+          onClick={handleClick}
+          type="button"
+        >
+          {!subscription ? "Follow" : "Following"}
         </button>
       </div>
     </div>
